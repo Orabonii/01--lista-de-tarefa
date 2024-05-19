73bb5f0be56bee82af344d6a77b9e065df329ddf
kauã oraboni <70423924+Orabonii@users.noreply.github.com>
2024-05-19 16:50:18 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'; 
+
+import ListaDeTarefas from './components/Projeto01/Projeto01'; 
+import './App.css'
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <ListaDeTarefas /> 
+
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default App; 
